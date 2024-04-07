73de2eeb7948008b9228ce80212d4169aa11c880
Modestas Slivinskas <modestas.slivinskas@gmail.com>
2024-04-07 17:26:29 +0300

    add filter by type

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -3,7 +3,9 @@
         class="rounded-t-lg overflow-hidden border-t border-l border-r border-gray-400 px-3 py-10 bg-gray-200 flex justify-center">
         <div class="w-full max-w-md">
             <div class="w-full max-w-md">
-                <div v-for="error in errors" class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+                <div v-for="error in errors"
+                    class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"
+                    role="alert">
                     <span class="font-medium">Klaida!</span> {{ error }}
                 </div>
                 <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" @submit="formSubmit" ref="formRef">
@@ -57,18 +59,21 @@
                             <fieldset>
                                 <label class="block text-gray-700 text-sm font-bold mb-2">Pavojingas krovinys</label>
 
-                                <input v-model="fragile_goods" type="radio" name="fragile_goods" value="1" id="fragile_goods_yes"/>
-                                <label class="text-gray-700 text-sm font-bold mb-2 mr-2 ml-1" for="fragile_goods_yes">Taip</label>
+                                <input v-model="fragile_goods" type="radio" name="fragile_goods" value="1"
+                                    id="fragile_goods_yes" />
+                                <label class="text-gray-700 text-sm font-bold mb-2 mr-2 ml-1"
+                                    for="fragile_goods_yes">Taip</label>
 
-                                <input v-model="fragile_goods" type="radio" name="fragile_goods" value="0" id="fragile_goods_no"/>
-                                <label class="text-gray-700 text-sm font-bold mb-2 mr-2 ml-1" for="fragile_goods_no">Ne</label>
+                                <input v-model="fragile_goods" type="radio" name="fragile_goods" value="0"
+                                    id="fragile_goods_no" />
+                                <label class="text-gray-700 text-sm font-bold mb-2 mr-2 ml-1"
+                                    for="fragile_goods_no">Ne</label>
                             </fieldset>
                         </div>
                     </div>
                     <div class="flex items-center justify-between">
                         <button
-                            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline disabled:bg-slate-500"
-                            >
+                            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline disabled:bg-slate-500">
                             Skaičiuoti
                         </button>
                     </div>
@@ -96,23 +101,6 @@ export default {
     components: {
         ShipmentTable
     },
-    computed: {
-            isCarCarrierFormFilled() {
-                return !!this.num_cars && !!this.distance;
-            },
-            isCargoTruckFormFilled() {
-                return !!this.cargo_weight && !!this.distance && !!this.fragile_goods;
-            },
-            isFormFilled() {
-                if(this.transport_department == 'car_carrier') {
-                    return this.isCarCarrierFormFilled;
-                }
-                if(this.transport_department == 'cargo_truck') {
-                    return this.isCargoTruckFormFilled;
-                }
-                return false;
-            }
-    },
     methods: {
         formSubmit(e) {
             e.preventDefault();
@@ -122,11 +110,11 @@ export default {
                 transport_department: this.transport_department,
             };
 
-            if(this.transport_department == 'car_carrier') {
+            if (this.transport_department == 'car_carrier') {
                 formData.num_cars = this.num_cars;
                 formData.distance = this.distance;
             }
-            else if(this.transport_department == 'cargo_truck') {
+            else if (this.transport_department == 'cargo_truck') {
                 formData.cargo_weight = this.cargo_weight;
                 formData.distance = this.distance;
                 formData.fragile_goods = this.fragile_goods;
@@ -137,26 +125,26 @@ export default {
             }
 
             this.axios.post('http://localhost:8000/calculate', formData)
-            .then(function (response) {
-                if(response.data.errors) {
-                    currentObj.errors = response.data.errors;
-                }
-                else {
-                    currentObj.refresh();
-                }
-            })
-            .catch(function (error) {
-                if(typeof(error.response) == "undefined") {
-                    return;
-                }
-                Object.values(error.response.data.errors)[0][0]
-                if(typeof(error.response.data.errors) !== 'undefined') {
-                    currentObj.errors = [];
-                    Object.values(error.response.data.errors).forEach((el) =>
-                        currentObj.errors.push(el[0])
-                    );
-                }
-            });
+                .then(function (response) {
+                    if (response.data.errors) {
+                        currentObj.errors = response.data.errors;
+                    }
+                    else {
+                        currentObj.refresh();
+                    }
+                })
+                .catch(function (error) {
+                    if (typeof (error.response) == "undefined") {
+                        return;
+                    }
+                    Object.values(error.response.data.errors)[0][0]
+                    if (typeof (error.response.data.errors) !== 'undefined') {
+                        currentObj.errors = [];
+                        Object.values(error.response.data.errors).forEach((el) =>
+                            currentObj.errors.push(el[0])
+                        );
+                    }
+                });
         },
         refresh() {
             this.shipmentTableKey += 1;
--- resources/js/ShipmentTable.vue
+++ resources/js/ShipmentTable.vue
@@ -1,48 +1,84 @@
 <template>
-    <div class="shadow-sm overflow-hidden my-5">
-        <table v-if="shipments.length != 0" class="border-collapse table-fixed w-full text-sm">
+    <div v-if="shipments.length != 0" class="shadow-sm overflow-hidden my-5">
+        <div class="w-full">
+            <div class="grid grid-cols-4 pb-3">
+                <div class="col-span-1"></div>
+                <div class="col-span-1">
+                    <input type="text" placeholder="Filtruoti pagal tipą" v-model="filter"
+                        class="block p-2 ps-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50" />
+                </div>
+                <div class="col-span-1"></div>
+                <div class="col-span-1"></div>
+            </div>
+        </div>
+        <table class="border-collapse table-fixed w-full text-sm">
             <thead>
-            <tr>
-                <th class="border-b dark:border-slate-600 font-medium p-3 pl-8 pt-0 text-slate-400 dark:text-slate-200 text-left">Data</th>
-                <th class="border-b dark:border-slate-600 font-medium p-3 pt-0 text-slate-400 dark:text-slate-200 text-left">Tipas</th>
-                <th class="border-b dark:border-slate-600 font-medium p-3 pr-8 pt-0 text-slate-400 dark:text-slate-200 text-left">Krovinio informacija</th>
-                <th class="border-b dark:border-slate-600 font-medium p-3 pr-8 pt-0 text-slate-400 dark:text-slate-200 text-left">Pervežimo kaina</th>
-            </tr>
+                <tr>
+                    <th
+                        class="border-b dark:border-slate-600 font-medium p-3 pl-8 pt-0 text-slate-400 dark:text-slate-200 text-left">
+                        Data</th>
+                    <th
+                        class="border-b dark:border-slate-600 font-medium p-3 pt-0 text-slate-400 dark:text-slate-200 text-left">
+                        Tipas</th>
+                    <th
+                        class="border-b dark:border-slate-600 font-medium p-3 pr-8 pt-0 text-slate-400 dark:text-slate-200 text-left">
+                        Krovinio informacija</th>
+                    <th
+                        class="border-b dark:border-slate-600 font-medium p-3 pr-8 pt-0 text-slate-400 dark:text-slate-200 text-left">
+                        Pervežimo kaina</th>
+                </tr>
             </thead>
             <tbody class="bg-white dark:bg-slate-800">
-            <tr v-for="shipment in shipments">
-                <td class="border-b border-slate-100 dark:border-slate-700 p-2 pl-8 text-slate-500 dark:text-slate-400">{{ shipment.created_at }}</td>
-                <td class="border-b border-slate-100 dark:border-slate-700 p-2 text-slate-500 dark:text-slate-400">{{ shipment.transport_department }}</td>
-                <td class="border-b border-slate-100 dark:border-slate-700 p-2 pr-8 text-slate-500 dark:text-slate-400">{{ shipment.cargoData }}</td>
-                <td class="border-b border-slate-100 dark:border-slate-700 p-2 pr-8 text-slate-500 dark:text-slate-400">{{ shipment.price }} EUR</td>
-            </tr>
+                <tr v-for="shipment in filteredShipments">
+                    <td
+                        class="border-b border-slate-100 dark:border-slate-700 p-2 pl-8 text-slate-500 dark:text-slate-400">
+                        {{ shipment.created_at }}</td>
+                    <td class="border-b border-slate-100 dark:border-slate-700 p-2 text-slate-500 dark:text-slate-400">
+                        {{ shipment.transport_department }}</td>
+                    <td
+                        class="border-b border-slate-100 dark:border-slate-700 p-2 pr-8 text-slate-500 dark:text-slate-400">
+                        {{ shipment.cargoData }}</td>
+                    <td
+                        class="border-b border-slate-100 dark:border-slate-700 p-2 pr-8 text-slate-500 dark:text-slate-400">
+                        {{ shipment.price }} EUR</td>
+                </tr>
             </tbody>
         </table>
     </div>
-  </template>
+</template>
 
-  <script>
-  import axios from 'axios';
+<script>
+import axios from 'axios';
 
-  export default {
+export default {
     data() {
-      return {
-        shipments: []
-      };
+        return {
+            shipments: [],
+            filter: '',
+        };
+    },
+    computed: {
+        filteredShipments() {
+            return this.shipments.filter(shipment => {
+                const type = shipment.transport_department.toString().toLowerCase();
+                const searchTerm = this.filter.toLowerCase();
+                return type.includes(searchTerm);
+            });
+        }
     },
     created() {
-      this.fetchShipments();
+        this.fetchShipments();
     },
     methods: {
-      fetchShipments() {
-        axios.get('/api/shipments')
-          .then(response => {
-            this.shipments = response.data;
-          })
-          .catch(error => {
-            console.error('Error fetching shipments:', error);
-          });
-      }
+        fetchShipments() {
+            axios.get('/api/shipments')
+                .then(response => {
+                    this.shipments = response.data;
+                })
+                .catch(error => {
+                    console.error('Error fetching shipments:', error);
+                });
+        }
     }
-  };
-  </script>
+};
+</script>
