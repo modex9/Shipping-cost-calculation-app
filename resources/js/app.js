40c780d7ac617e2ffd83cc08e3ff4abb01e059bc
Modestas Slivinskas <modestas.slivinskas@gmail.com>
2024-06-09 07:16:34 +0300

    remove unused components

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,12 +3,10 @@ import './bootstrap';
 import { createApp, h } from "vue";
 import { createInertiaApp } from '@inertiajs/inertia-vue3';
 import { InertiaProgress } from '@inertiajs/progress';
-import App from "./App.vue";
 import axios from 'axios';
 import VueAxios from 'vue-axios';
 
 InertiaProgress.init();
-// createApp(App).use(VueAxios, axios).mount("#app");
 createInertiaApp({
     resolve: name => {
         const pages = import.meta.glob('./Components/*.vue', { eager: true })
